bbafd8953a748cb069a06ae597b157a25f75bcb9
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-05-27 01:47:03 +0200

    Save unordered_set version of exo3

--- src/06_optimization/ex03/HostCounter.cpp
+++ src/06_optimization/ex03/HostCounter.cpp
@@ -1,26 +1,20 @@
 #include "HostCounter.h"
 
-#include <algorithm> // for std::find
+#include <algorithm>  // for std::find
 
-HostCounter::HostCounter()
-{
-}
+HostCounter::HostCounter() {}
 
 bool HostCounter::isNewHost(std::string hostname)
 {
-    return std::find(myHosts.begin(), myHosts.end(), hostname) == myHosts.end();
+    return myHosts.find(hostname) == myHosts.end();
 }
 
 void HostCounter::notifyHost(std::string hostname)
 {
     // add the host in the list if not already in
-    if(isNewHost(hostname))
-    {
-        myHosts.push_back(hostname);
+    if (isNewHost(hostname)) {
+        myHosts.insert(hostname);
     }
 }
 
-int HostCounter::getNbOfHosts()
-{
-    return myHosts.size();
-}
+int HostCounter::getNbOfHosts() { return myHosts.size(); }
--- src/06_optimization/ex03/HostCounter.h
+++ src/06_optimization/ex03/HostCounter.h
@@ -1,22 +1,22 @@
+#include <set>
 #include <string>
-
+#include <unordered_set>
 #include <vector>
 
-class HostCounter
-{
-    public:
-        HostCounter();
+class HostCounter {
+   public:
+    HostCounter();
 
-        // Announce a host to the HostCounter.
-        // if the host is new, it will be added to the list, otherwise we ignore it.
-        void notifyHost(std::string hostname);
+    // Announce a host to the HostCounter.
+    // if the host is new, it will be added to the list, otherwise we ignore it.
+    void notifyHost(std::string hostname);
 
-        // return the number of unique hosts found so far
-        int getNbOfHosts();
+    // return the number of unique hosts found so far
+    int getNbOfHosts();
 
-    private:
-        // check if host is already in the list
-        bool isNewHost(std::string hostname);
+   private:
+    // check if host is already in the list
+    bool isNewHost(std::string hostname);
 
-        std::vector< std::string > myHosts;
+    std::unordered_set<std::string> myHosts;
 };
