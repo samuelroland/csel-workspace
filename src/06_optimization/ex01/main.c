5b6df919253aa20145b9c9e514e8ab8e48e0e345
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-05-27 01:28:30 +0200

    Add improved version of exo1

--- src/06_optimization/ex01/main.c
+++ src/06_optimization/ex01/main.c
@@ -4,20 +4,15 @@
 
 static int32_t array[SIZE][SIZE];
 
-int main (void)
-{
+int main(void) {
     int i, j, k;
 
-    for (k = 0; k < 10; k++)
-    {
-        for (i = 0; i < SIZE; i++)
-        {
-            for (j = 0; j < SIZE; j++)
-            {
-                array[j][i]++;
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            for (k = 0; k < 10; k++) {
+                array[i][j]++;
             }
         }
     }
     return 0;
 }
-
