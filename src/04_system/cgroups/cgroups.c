06ad52bb6e6f20bdf58e26710aaad4a4ad3cbf83
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-05-26 18:18:14 +0200

    Add cgroups program to test memory allocation

--- src/04_system/cgroups/cgroups.c
+++ src/04_system/cgroups/cgroups.c
@@ -1,4 +1,29 @@
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[]) { return 0; }
+#define MEBIBYTE (1 << 20)
+size_t total_allocated = 0;
+
+// Return 2^20 bytes of data and return true of allocation succeeded
+bool allocate_mebibyte(size_t count) {
+    size_t size = count * MEBIBYTE;
+    void* ptr = malloc(size);
+    if (ptr == NULL) return false;
+    total_allocated += size;
+    memset(ptr, 0, size);
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    while (allocate_mebibyte(1)) {
+        printf("Allocated 1 MEBIBYTE, reaching a total of %lu bytes\n",
+               total_allocated);
+        usleep(100000);
+    }
+    printf("Allocation failed, reaching a total of %lu bytes\n",
+           total_allocated);
+}
