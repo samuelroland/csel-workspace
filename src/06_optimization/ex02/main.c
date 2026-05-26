d717f20624dd4bf232118458cb9d6fa3256fb788
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2026-05-27 01:26:58 +0200

    Add optimized version of exo2

--- src/06_optimization/ex02/main.c
+++ src/06_optimization/ex02/main.c
@@ -3,22 +3,23 @@
 
 #define SIZE 65536
 
-int main()
-{
- 	// generate data
-	short data[SIZE];
-	for (int i = 0; i < SIZE; i++) {
-		data[i] = rand() % 512;
-	}
-
-
-	long long sum = 0;
-	for (int j = 0; j < 10000; j++) {
-		for (int i = 0; i < SIZE; i++) {
-			if (data[i] >= 256) {
-				sum += data[i];
-			}
-		}
-	}
-	printf ("sum=%lld\n", sum);
+static int compare(const void* a, const void* b) {
+    return *(short*)a - *(short*)b;
+}
+int main() {
+    // generate data
+    short data[SIZE];
+    for (int i = 0; i < SIZE; i++) {
+        data[i] = rand() % 512;
+    }
+    qsort(data, SIZE, sizeof(data[0]), compare);
+    long long sum = 0;
+    for (int j = 0; j < 10000; j++) {
+        for (int i = 0; i < SIZE; i++) {
+            if (data[i] >= 256) {
+                sum += data[i];
+            }
+        }
+    }
+    printf("sum=%lld\n", sum);
 }
